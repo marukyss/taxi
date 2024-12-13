a9c924defa9ae1b1f99fe7d382567baf04ec3ebb
u7i <umnicode2@gmail.com>
2024-12-13 15:12:19 +0200

    Add: DB wiping

--- src/db/engine.py
+++ src/db/engine.py
@@ -1,6 +1,8 @@
 import asyncio
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine
+from sqlalchemy import ForeignKeyConstraint, inspect
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine, AsyncConnection
+from sqlalchemy.sql.ddl import DropConstraint, DropTable
 
 from db.base_model import BaseSqlModel
 
@@ -11,7 +13,7 @@ class SqlEngine:
     """
 
     def __init__(self, url: str):
-        """Create a new engine from the given URL.
+        """Creates a new engine from the given URL.
         Should be invoked when a server needs to connect to another database.
         """
 
@@ -25,6 +27,35 @@ class SqlEngine:
 
         async with self.__engine.begin() as conn:
             await conn.run_sync(BaseSqlModel.metadata.create_all)
+
+    async def drop_all(self) -> None:
+        """Deletes ALL the tables known to the application.
+        Should be called with an EXTREME caution.
+        """
+
+        # Drop all the tables & the FK constraints
+        async with self.__engine.begin() as conn:
+            tables = []
+            fk_constrains = []
+
+            # Discover the DB structure
+            for tbl in BaseSqlModel.metadata.tables.values():
+                fk_list = await conn.run_sync(
+                    lambda sync_conn: inspect(sync_conn).get_foreign_keys(tbl.name)
+                )
+
+                for fk in fk_list:
+                    fk_constrains.append(ForeignKeyConstraint((), (), table=tbl, name=fk["name"]))
+
+                tables.append(tbl)
+
+            # Drop all the FK constrains at fist
+            for fk in fk_constrains:
+                await conn.execute(DropConstraint(fk))
+
+            # Drop the tables freed from constrains
+            for tbl in tables:
+                await conn.execute(DropTable(tbl))
 
     def session_maker(self) -> async_sessionmaker:
         """Returns a session-maker for this DB engine."""
